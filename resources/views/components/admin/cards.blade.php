182da52f483d5a6dbdb63d949416fa4b1adcf3bb
arpit mishra <arpit.mishra.out@gmail.com>
2024-11-18 23:19:16 +0530

    components in laravel

---
+++ resources/views/components/admin/cards.blade.php
@@ -0,0 +1,15 @@
+{{-- <div class="card">
+    <h3>Admin Card Component</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia distinctio odit numquam pariatur quasi porro, consequatur voluptatum exercitationem placeat iste eos. Nesciunt odio provident nostrum ullam quo. Amet minima harum rerum voluptatem quidem sed excepturi consectetur eos reprehenderit? Ipsa dolor et vel laboriosam reprehenderit enim.</p>
+</div> --}}
+
+
+ <div class="card">
+     <h3>{{ $title }}</h3>
+     @if ($slot->isEmpty())
+         <p>Please add content to the slot.</p>
+     @else
+        {{ $slot }}
+     @endif
+     <h3>{{ $footer }}</h3>
+</div>
---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- He who is contented is rich. - Laozi -->
+    <button>
+        Submit four card details
+    </button>
+</div>
